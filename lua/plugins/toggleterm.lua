02c621f6ea08334ccc838e150e3828cc97fb435b
Prastowo Agung Widodo <prastowoagungwidodo@gmail.com>
2023-11-14 19:21:27 +0700

    fix toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -7,7 +7,7 @@ Plugin.keys = {'<C-g>'}
 -- See :help toggleterm-roadmap
 Plugin.opts = {
   open_mapping = '<C-g>',
-  direction = 'float',
+  direction = 'horizontal',
   float_opts = {
     border = 'curved',
   },
